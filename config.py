57a1823f0cd27f7d3a9a79c8139f9f6386073c2a
LaurelSSP <131743413+LaurelSSP@users.noreply.github.com>
2024-10-25 17:11:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = '7868426310:AAHOf_dNqe9w_vJ03VS779kQOxezGulqjMc'
-ADMIN_IDS = [7097239708]
-WORKER_ID = '7097239708'
-ADMIN_USERNAME = 'fastsfateg'
+BOT_TOKEN = '' # ВАШ ТОКЕН
+ADMIN_IDS = [111222333, 333222111] # ID Администраторов, кто имеет доступ к настройки
+WORKER_ID = '111222333' #  ID обработчика заявок
+ADMIN_USERNAME = 'fastsfateg' # USERNAME обработчика заявок 
 CAPTCHA_TIMEOUT = 15
 COMMISSION_RATE = 2.5
